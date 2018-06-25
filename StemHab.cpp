d24e11f2c24e66b9b9018f6a04d76c4434499aa3
Joshua Grebler <jgreb314@gmail.com>
2018-06-24 22:43:44 -0700

    Moved GPS code back and made GPS initialization variable (startGPS now takes rx/tx arguments) Removed routine to return old data when connection lost

--- StemHab.cpp
+++ StemHab.cpp
@@ -1,6 +1,7 @@
 // Stem High Altitude Balloon Project 2017
 // Modified for Stem High Altitude Balloon 2018 by Joshua Grebler
 
+#include <math.h> 
 // Temperature 
 // resistance at 25 degrees C
 #define THERMISTORNOMINAL 1020 
@@ -46,4 +47,87 @@ float getTempF( float tempC ) {
 float getPressure( int rawValue ) {
 	float pressure = ((rawValue * 0.0049 -.33)/1.65)*100;
 	return pressure;
+}
+
+// GPS Utility Functions Start
+
+#define GPS_TX 2
+#define GPS_RX 3
+//
+
+Adafruit_GPS GPS(&mySerial);
+void startGPS(int gps_tx, int gps_rx) {
+  SoftwareSerial mySerial(gps_tx, gps_rx); //The Adalogger 32u4 has no available hardware serial pins, so we use SoftwareSerial
+  GPS.begin(4800);
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  OCR0A = 0xAF;
+  TIMSK0 |= _BV(OCIE0A);
+}
+
+String getGpsData() {
+  if (!GPS.parse(GPS.lastNMEA() )  ) {
+    Serial.println("No connection");
+    return "0,0,0,0,0,0,0,0,0,0,0,0";
+  }
+  
+  String str = "";
+  
+  str += GPS.hour;
+  str += ":";
+  
+  str += GPS.minute;
+  str += ":";
+  
+  str += GPS.seconds;
+  str += ",";
+  
+  str += GPS.month;
+  str += "/";
+  
+  str += GPS.day;
+  str += "/";
+  
+  str += GPS.year;
+  str += ",";
+  
+  str += (int) GPS.fix;
+  str += ",";
+  
+  str += (int) GPS.fixquality;
+  str += ",";
+  
+  str += GPS.latitude;
+  str += GPS.lat;
+  str += ",";
+  
+  str += GPS.longitude;
+  str += GPS.lon;
+  str += ",";
+  
+  str += GPS.latitudeDegrees;
+  str += ",";
+  
+  str += GPS.longitudeDegrees;
+  str += ",";
+  
+  str += GPS.altitude;
+  str += ",";
+  
+  str += GPS.speed;
+  str += ",";
+  
+  str += GPS.angle;
+  str += ",";
+  
+  
+  str += (int)GPS.satellites;
+  
+  oldGpsData = str;
+  return str;
+}
+
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
 }
