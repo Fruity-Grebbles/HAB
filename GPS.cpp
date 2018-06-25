e53c37f360b8409c83245f6a5621da879503db7a
Joshua Grebler <jgreb314@gmail.com>
2018-06-25 00:07:45 -0700

    GPS routine implemented as class

--- GPS.cpp
+++ GPS.cpp
@@ -1,77 +1,63 @@
+#include <Adafruit_GPS.h>
 
-void startGPS(int gps_tx, int gps_rx) {
+class myGPS {
+  private:
+    Adafruit_GPS* GPS;
+  public:
+    myGPS(int gps_tx, int gps_rx);
+    String getGpsData();
+    void handler();
+};
+
+myGPS::myGPS(int gps_tx, int gps_rx) {
   SoftwareSerial mySerial(gps_tx, gps_rx); //The Adalogger 32u4 has no available hardware serial pins, so we use SoftwareSerial
-  Adafruit_GPS GPS(&mySerial);
-  GPS.begin(4800);
+  GPS = new Adafruit_GPS(&mySerial);
+  GPS->begin(4800);
   // Set the update rate
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  GPS->sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
   OCR0A = 0xAF;
   TIMSK0 |= _BV(OCIE0A);
+  // Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+  //attatchInterrupt(TIMER0_COMPA_vect, handler);
 }
 
-String getGpsData() {
-  if (!GPS.parse(GPS.lastNMEA() )  ) {
+String myGPS::getGpsData() {
+  if (!GPS->parse(GPS->lastNMEA() )  ) {
     Serial.println("No connection");
     return "0,0,0,0,0,0,0,0,0,0,0,0";
   }
-  
   String str = "";
-  
-  str += GPS.hour;
+
+  str += GPS->hour;
   str += ":";
-  
-  str += GPS.minute;
+  str += GPS->minute;
   str += ":";
-  
-  str += GPS.seconds;
+  str += GPS->seconds;
   str += ",";
-  
-  str += GPS.month;
+  str += GPS->month;
   str += "/";
-  
-  str += GPS.day;
-  str += "/";
-  
-  str += GPS.year;
+  str += GPS->day + "/";
+  str += GPS->year + ",";
+  str += (int) GPS->fix + ",";
+  str += (int) GPS->fixquality + ",";
+  str += GPS->latitude;
+  str += GPS->lat + ",";
+  str += GPS->longitude;
+  str += GPS->lon;
   str += ",";
-  
-  str += (int) GPS.fix;
+  str += GPS->latitudeDegrees;
   str += ",";
-  
-  str += (int) GPS.fixquality;
+  str += GPS->longitudeDegrees;
   str += ",";
-  
-  str += GPS.latitude;
-  str += GPS.lat;
+  str += GPS->altitude;
   str += ",";
-  
-  str += GPS.longitude;
-  str += GPS.lon;
+  str += GPS->speed;
   str += ",";
-  
-  str += GPS.latitudeDegrees;
+  str += GPS->angle;
   str += ",";
-  
-  str += GPS.longitudeDegrees;
-  str += ",";
-  
-  str += GPS.altitude;
-  str += ",";
-  
-  str += GPS.speed;
-  str += ",";
-  
-  str += GPS.angle;
-  str += ",";
-  
-  
-  str += (int)GPS.satellites;
-  
-  oldGpsData = str;
+  str += (int)GPS->satellites;
   return str;
 }
-
-// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
-SIGNAL(TIMER0_COMPA_vect) {
-  char c = GPS.read();
+static void myGPS::handler() {
+  char c = GPS->read();
 }
