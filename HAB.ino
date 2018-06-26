0d54fbe72f9fe8cbcac44d2c88204893a045a8b2
francose <elmanoukdesign@gmail.com>
2018-06-26 17:18:21 -0400

    This is an initial commit

--- HAB.ino
+++ HAB.ino
@@ -1,3 +1,18 @@
-void setup() {}
 
-void loop() {}
+
+#include "GPS.h"
+
+void setup() {
+  Serial.begin(9600);
+  String getGpsData(11,12);
+ 
+
+
+
+
+
+
+}
+void loop() {
+
+}
---
+++ headers/GPS.h
@@ -0,0 +1,8 @@
+#ifndef GPS
+#define GPS
+
+
+// myGPS test(10,11);
+String  getGpsData();
+
+#endif
