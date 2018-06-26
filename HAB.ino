de3de5abc6f8466b1b39974d743a367fd47bcc44
Joshua Grebler <jgreb314@gmail.com>
2018-06-26 15:27:31 -0700

    SD card logging code (currently empty)

--- HAB.ino
+++ HAB.ino
@@ -1,3 +1,27 @@
-void setup() {}
+#include <SPI.h>
+#include <SD.h>
 
-void loop() {}
+const int chipSelect = 4;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    return;
+  }
+  Serial.println("card initialized.");
+}
+
+void loop() {
+  String dataString = "";
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+  }
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+}
