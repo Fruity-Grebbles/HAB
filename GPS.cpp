f3da6c4b7858f89dd33052585ab20e84b8910f48
Joshua Grebler <jgreb314@gmail.com>
2018-07-03 14:09:13 -0700

    Removed testing lines

--- GPS.cpp
+++ GPS.cpp
@@ -7,22 +7,20 @@
 
 using namespace std;
 
- myGPS::myGPS(int gps_tx, int gps_rx) {
-  Serial.print("test from mygps");
-  return ;
+myGPS::myGPS(int gps_tx, int gps_rx) {
   SoftwareSerial mySerial(gps_tx, gps_rx); //The Adalogger 32u4 has no available hardware serial pins, so we use SoftwareSerial
 
-   GPS = new Adafruit_GPS(&mySerial);
-   GPS->begin(4800);
- // Set the update rate
+  GPS = new Adafruit_GPS(&mySerial);
+  GPS->begin(4800);
+  // Set the update rate
 
-   GPS->sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
-   OCR0A = 0xAF;
-   TIMSK0 |= _BV(OCIE0A);
+  GPS->sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  OCR0A = 0xAF;
+  TIMSK0 |= _BV(OCIE0A);
 
-   // Interrupt is called once a millisecond, looks for any new GPS data, and stores it
-   //attatchInterrupt(TIMER0_COMPA_vect, handler);
- }
+  // Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+  //attatchInterrupt(TIMER0_COMPA_vect, handler);
+}
 //
 String myGPS::getGpsData() {
   if (!GPS->parse(GPS->lastNMEA() )  ) {
@@ -30,7 +28,7 @@ String myGPS::getGpsData() {
     return "0,0,0,0,0,0,0,0,0,0,0,0";
   }
   String str = "";
-//
+  //
   str += GPS->hour;
   str += ":";
   str += GPS->minute;
