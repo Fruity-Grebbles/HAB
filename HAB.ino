58607dfa266d7759d0803d79be83d48cc00e237f
Joshua Grebler <jgreb314@gmail.com>
2018-07-09 16:01:57 -0700

    Reconfigured GPS handling following example by ladyada

--- HAB.ino
+++ HAB.ino
@@ -1,54 +1,112 @@
+// Based on Ladyada & Bill Greimanfor's logger
+// for the Adafruit Ultimate GPS Shield
+
 #include <SPI.h>
 #include <SD.h>
-#include "GPS.h"
+#include <SoftwareSerial.h>
 #include <Adafruit_GPS.h>
-#include <WString.h>
+
 #define MOTOR_TRIGGER_ALTITUDE 250
 
 const int chipSelect = 4;
-myGPS module(11, 12);
 bool motor_flag = false;
 
-// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
-SIGNAL(TIMER0_COMPA_vect) {
-  char c = module.GPS->read();
-}
+SoftwareSerial mySerial(11, 12);
+Adafruit_GPS GPS(&mySerial);
 
+File logfile;
 
 void setup() {
-  Serial.begin(9600);
-
+  Serial.begin(115200);
   Serial.print("Initializing SD card...");
+
+  pinMode(chipselect, OUTPUT);
   if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
     return;
   }
   Serial.println("card initialized.");
+
+  char filename[15];
+  strcpy(filename, "GPSLOG00.TXT");
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[6] = '0' + i / 10;
+    filename[7] = '0' + i % 10;
+    // create if does not exist, do not open existing, write, sync after write
+    if (! SD.exists(filename)) {
+      break;
+    }
+  }
+
+  logfile = SD.open(filename, FILE_WRITE);
+  if ( ! logfile ) {
+    Serial.print("Couldnt create ");
+    Serial.println(filename);
+  }
+  Serial.print("Writing to ");
+  Serial.println(filename);
+
+  GPS.begin(9600);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1Hz update rate
+  GPS.sendCommand(PGCMD_NOANTENNA); // Turn off updates on antenna status
+  
+  // enable interrupt
+  OCR0A = 0xAF;
+  TIMSK0 |= _BV(OCIE0A);
+
+  Serial.println("Ready!");
+}
+
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+  #ifdef UDR0
+        if (c) UDR0 = c;  
+      // writing direct to UDR0 is much much faster than Serial.print 
+      // but only one character can be written at a time. 
+  #endif
 }
 
 void loop() {
-  String dataString = "";
-  Serial.println(module.getGpsData());
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  String dataString = "Hello World";
+  //Serial.println(module.getGpsData());
+}
 
-  if (dataFile) {
-    dataFile.println(dataString);
-
-    //Motor handling logic
-    if (module.GPS->altitude > MOTOR_TRIGGER_ALTITUDE) {
-      if (!motor_flag) {
-        motor_flag = true;
-        dataFile.println("Dishes opened");
-      }
-    } else {
-      if(motor_flag) {
-        dataFile.println("Dishes closed");
-      }
-    }
-
-    dataFile.close();
+String getGpsData() {
+  if (!GPS.parse(GPS.lastNMEA() )  ) {
+    Serial.println("No connection");
+    return "0,0,0,0,0,0,0,0,0,0,0,0";
   }
-  else {
-    Serial.println("error opening datalog.txt");
-  }
+  String str = "";
+  str += GPS.hour;
+  str += ":";
+  str += GPS.minute;
+  str += ":";
+  str += GPS.seconds;
+  str += ",";
+  str += GPS.month;
+  str += "/";
+  str += GPS.day + "/";
+  str += GPS.year + ",";
+  str += (int) GPS.fix + ",";
+  str += (int) GPS.fixquality + ",";
+  str += GPS.latitude;
+  str += GPS.lat + ",";
+  str += GPS.longitude;
+  str += GPS.lon;
+  str += ",";
+  str += GPS.latitudeDegrees;
+  str += ",";
+  str += GPS.longitudeDegrees;
+  str += ",";
+  str += GPS.altitude;
+  str += ",";
+  str += GPS.speed;
+  str += ",";
+  str += GPS.angle;
+  str += ",";
+  str += (int)GPS.satellites;
+  return str;
 }
